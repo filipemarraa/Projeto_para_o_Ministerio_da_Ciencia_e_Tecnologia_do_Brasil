3a1c88d3585379073c07ac3c2a2230028b6c378e
Filipe Jacobson Marra <filipejacobsonmarra@gmail.com>
2023-11-01 11:52:23 -0300

    Descompactando os arquivos
    Nesse codigo descompactamos os arquivos .zip. 
    (.zip -> .csv)
    Com esse codigo a automação para para as planilhas fica algo mais facil de ser feito.

---
+++ descompactar2.py
@@ -0,0 +1,19 @@
+import os
+from zipfile import ZipFile
+
+caminho = "/Users/filipemarra/Desktop/Projetos/Web_Scrapping"
+zip_encontrado = False
+
+for item in os.listdir(caminho):
+    if item.endswith('.zip'):
+        zip_encontrado = True
+        try:
+            arq = ZipFile(os.path.join(caminho, item), 'r')
+            arq.extractall(caminho)  # Aqui, o segundo argumento define o diretório para onde extrair os arquivos
+            arq.close()
+            print(f"O arquivo {item} foi descompactado com sucesso!!")
+        except Exception as e:
+            print(f"Erro ao descompactar o arquivo {item}: {e}")
+
+if not zip_encontrado:
+    print("Nenhum arquivo .zip encontrado no diretório.")
