a4cf1a3cdfa588915663183abd803684921cd3be
Filipe Jacobson Marra <filipejacobsonmarra@gmail.com>
2023-11-24 09:15:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -37,10 +37,55 @@ if response.status_code == 200:
                     caminho_para_csv_original = os.path.join(caminho_base, 'siconv_convenio.csv')  # Ajuste o nome do arquivo CSV conforme o real nome do arquivo extraído
                     df = pd.read_csv(caminho_para_csv_original)
 
-                    # Definição das colunas desejadas e filtragem do DataFrame
-                    colunas_desejadas = ['NR_CONVENIO', 'ID_PROPOSTA', 'SIT_CONVENIO']
-                    df_filtrado = df[colunas_desejadas]
-                    df_filtrado = df_filtrado[df_filtrado['SIT_CONVENIO'] == 'Cancelado']
+                    # Linhas específicas a serem filtradas
+                    linhas_especificas = [
+  
+    "01245003244202258",
+    "01245007491202142",
+    "01245007439202196",
+    "01245001336202201",
+    "01245006778202317",
+    "01245004365202306",
+    "01245004141202396",
+    "01245009771202357",
+    "01250025586201954",
+    "01250025593201956",
+    "01245010248202110",
+    "01250025767201981",
+    "01245010265202149",
+    "01250030184201891",
+    "01200004959201515",
+    "01250025606201997",  
+    "01245010317202187",
+    "01245004437202315",
+    "01245001333202260",
+    "01245001329202200",
+    "01245001328202257",
+    "01245007444202107",
+    "01250016357202082",
+    "01245004439202304",
+    "01200001217201619",
+    "01245003205202251",
+    "01245004338202325",
+    "01245005479202365",
+    "01250030075201792",
+    "01200002053201485",
+    "01200002337201048",
+    "01200007298200770",
+    "01200004693200881",
+    "01250024988202075",
+]
+
+                    # Colunas desejadas
+                    colunas_desejadas = [
+                        'NR_CONVENIO', 'ID_PROPOSTA', 'SIT_CONVENIO', 'NR_PROCESSO',
+                        'DIA_INIC_VIGENC_CONV', 'DIA_FIM_VIGENC_CONV', 'DIA_FIM_VIGENC_ORIGINAL_CONV',
+                        'DIAS_PREST_CONTAS', 'DIA_LIMITE_PREST_CONTAS', 'QTD_TA',
+                        'VL_GLOBAL_CONV', 'VL_REPASSE_CONV', 'VL_CONTRAPARTIDA_CONV', 'VL_EMPENHADO_CONV'
+                    ]
+
+                    # Filtragem do DataFrame
+                    df_filtrado = df[df['NR_CONVENIO'].isin(linhas_especificas)][colunas_desejadas]
 
                     # Adição da data e hora da última modificação
                     fuso_horario_brasilia = pytz.timezone('America/Sao_Paulo')
@@ -61,6 +106,7 @@ if response.status_code == 200:
                 print(f"Falha ao baixar o arquivo {file_url}.")
 else:
     print("Falha ao acessar a página.")
+
 
 
 
