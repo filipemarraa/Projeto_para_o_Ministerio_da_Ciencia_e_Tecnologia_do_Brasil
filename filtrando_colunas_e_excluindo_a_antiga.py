2398fcc028ed8ee9d6e44371a8e330c6bee96af6
LucasRD3105 <lucasrdprofissional@gmail.com>
2023-11-28 10:13:54 -0300

    alterando planilha de filtragem

--- filtrando_colunas_e_excluindo_a_antiga.py
+++ filtrando_colunas_e_excluindo_a_antiga.py
@@ -13,13 +13,27 @@ caminho_para_csv_original = "/Users/filipemarra/Desktop/Projetos/Web_Scrapping/s
 df = pd.read_csv(caminho_para_csv_original)
 
 # Lista as colunas que deseja manter
-colunas_desejadas = ['NR_CONVENIO', 'ID_PROPOSTA', 'SIT_CONVENIO']
+colunas_desejadas = ['NR_CONVENIO', 'ID_PROPOSTA', 'SIT_CONVENIO', 'NR_PROCESSO', 'DIA_INIC_VIGENC_CONV', 
+                     'DIA_FIM_VIGENC_CONV', 'DIA_FIM_VIGENC_ORIGINAL_CONV', 'DIAS_PREST_CONTAS', 
+                     'DIA_LIMITE_PREST_CONTAS', 'QTD_TA', 'VL_GLOBAL_CONV', 'VL_REPASSE_CONV', 
+                     'VL_CONTRAPARTIDA_CONV', 'VL_EMPENHADO_CONV']
 
 # Filtra o DataFrame para incluir apenas as colunas desejadas
 df_filtrado = df[colunas_desejadas]
 
-# Filtra as linhas baseando-se na condição desejada
-df_filtrado = df_filtrado[df_filtrado['SIT_CONVENIO'] == 'Cancelado']
+# Lista dos números de convênio desejados
+numeros_convenio = ['01245010317202187', '01245003244202258', '01245007491202142', '01245007439202196', 
+                    '01245001336202201', '01245006778202317', '01245004365202306', '01245004141202396', 
+                    '01245009771202357', '01250025586201954', '01250025593201956', '01245010248202110', 
+                    '01250025767201981', '01245010265202149', '01250030184201891', '01200004959201515', 
+                    '01250025606201997', '01245004437202315', '01245001333202260', '01245001329202200', 
+                    '01245001328202257', '01245007444202107', '01250016357202082', '01245004439202304', 
+                    '01200001217201619', '01245003205202251', '01245004338202325', '01245005479202365', 
+                    '01250030075201792', '01200002053201485', '01200002337201048', '01200007298200770', 
+                    '01200004693200881', '01250024988202075']
+
+# Filtra as linhas baseando-se nos números de convênio desejados
+df_filtrado = df_filtrado[df_filtrado['NR_CONVENIO'].isin(numeros_convenio)]
 
 # Obtém a data e hora atuais no fuso horário de Brasília
 data_hora_atual_brasilia = datetime.now(fuso_horario_brasilia).strftime("%Y-%m-%d %H:%M:%S")
